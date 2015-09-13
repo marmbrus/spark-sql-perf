dcf1d2ba193929b471fa51c250e8f22de8641958
Michael Armbrust <michael@databricks.com>
2015-09-13 00:08:06 -0700

    add bar_chart

--- src/main/notebooks/performance.dashboard.scala
+++ src/main/notebooks/performance.dashboard.scala
@@ -1,7 +1,5 @@
-// Databricks notebook source exported at Fri, 11 Sep 2015 06:27:08 UTC
+// Databricks notebook source exported at Sun, 13 Sep 2015 07:08:06 UTC
 import com.databricks.spark.sql.perf._
-
-// ali was here
 
 import org.apache.spark.sql._
 import org.apache.spark.sql.functions._
@@ -92,6 +90,10 @@ sqlContext.udf.register("gm", gm)
 
 // MAGIC %python spark_versions = ["1.6.0", "1.5.0", "1.4.1"]
 // MAGIC 
+// MAGIC import matplotlib.pyplot as plt
+// MAGIC import numpy as np
+// MAGIC from math import ceil, floor
+// MAGIC 
 // MAGIC def plot_single(df, column, height = 30):
 // MAGIC   for v in spark_versions:
 // MAGIC     runtimes = df[df['sparkVersion'] == v][column]
@@ -102,6 +104,66 @@ sqlContext.udf.register("gm", gm)
 // MAGIC 
 // MAGIC   plt.legend(spark_versions, loc='lower left')
 // MAGIC   plt.ylim([0,height])
+// MAGIC 
+// MAGIC 
+// MAGIC def bar_chart(df, graphCol, groupCol, colorCol, heightCol, colors = ['#29B1C1', '#EF6C30', '#21B16B', '#E8302D']):
+// MAGIC   graphs = df[graphCol].unique()
+// MAGIC   num_graphs = len(graphs)
+// MAGIC   rows = int(ceil(num_graphs / 2))
+// MAGIC 
+// MAGIC   fig, axarr = plt.subplots(num_graphs, figsize=(5, 15))
+// MAGIC 
+// MAGIC   for idx, graph in enumerate(graphs):
+// MAGIC     ax = axarr[idx]
+// MAGIC     data = df[df[graphCol] == graph]
+// MAGIC 
+// MAGIC     ax.set_title(graph)
+// MAGIC     groups = list(data[groupCol].unique())
+// MAGIC     num_groups = len(groups)
+// MAGIC     width = 0.3                  # the width of the bars
+// MAGIC     ind = np.arange(num_groups)  # the x locations for the groups
+// MAGIC     ax.set_xticklabels(groups)
+// MAGIC     ax.set_xticks(np.arange(num_groups) + (width * 1.5))
+// MAGIC 
+// MAGIC     color_values = sorted(list(df[colorCol].unique()))
+// MAGIC     rects = []
+// MAGIC     for i, color in enumerate(color_values):
+// MAGIC       data = df[df[colorCol] == color][df[graphCol] == graph]
+// MAGIC       print colors[i]
+// MAGIC       rect = ax.bar(ind, list(data[heightCol]), width, color=colors[i], yerr=list(data['error']), ecolor='black')
+// MAGIC       rects.append(rect)
+// MAGIC       ind = ind + width
+// MAGIC 
+// MAGIC     ax.legend( rects, color_values, loc='upper left')
+// MAGIC 
+// MAGIC   display(fig)
+
+// COMMAND ----------
+
+val groups = 
+  ("Interactive Queries", Seq("q19", "q42", "q52", "q55", "q63", "q68", "q73", "q98")) ::
+  ("Reporting Queries", Seq("q3","q7","q27","q43", "q53", "q89")) ::
+  ("Deep Analytics", Seq("q34", "q46", "q59", "q65",  "q79", "ss_max")) :: Nil
+
+groups.toDF("queryType", "queries").select($"queryType", explode($"queries").as("queryName")).registerTempTable("queryTypes")
+
+// COMMAND ----------
+
+// MAGIC %python 
+// MAGIC 
+// MAGIC from pyspark.sql.functions import expr
+// MAGIC 
+// MAGIC df = table("tpcds1500_smoothed").alias("data") \
+// MAGIC   .join(table("queryTypes"), expr("queryTypes.queryName = data.queryName")) \
+// MAGIC   .groupBy("queryType", "data.queryName", "sparkVersion") \
+// MAGIC   .agg( \
+// MAGIC     expr("avg(medianRuntimeSeconds) AS score"), \
+// MAGIC     expr("stddev(medianRuntimeSeconds) as error")) \
+// MAGIC   .orderBy("data.queryName") \
+// MAGIC   .toPandas()
+// MAGIC 
+// MAGIC #display(df)
+// MAGIC bar_chart(df, graphCol = 'queryType', groupCol='queryName', colorCol='sparkVersion', heightCol='score')
 
 // COMMAND ----------
 
@@ -205,10 +267,7 @@ org.apache.spark.SPARK_VERSION
 // COMMAND ----------
 
 val joins = new RecentRuns("joins", _.filter($"jobname" === "joins.daily"))
-
-// COMMAND ----------
-
-display(table("joins_smoothed").groupBy("runId", "sparkVersion").agg(sum("medianRuntimeSeconds").alias("runtime")))
+display(joins.recentRuns.select($"runId", $"sparkVersion", $"startTime", $"timestamp").distinct)
 
 // COMMAND ----------
 
@@ -238,11 +297,165 @@ display(table("joins_smoothed").groupBy("runId", "sparkVersion").agg(sum("median
 
 // COMMAND ----------
 
+import scala.util.Try
+
+val getNum = sqlContext.udf.register("getNum", (q: String) => Try(q.split(": ").last.toInt).getOrElse(0))
+val getParam = sqlContext.udf.register("getParam", (q: String) => q.split(": ").last)
+val getVariant = sqlContext.udf.register("getVariant", (q: String) => q.split(": ").head.split(" ").last)
+
+// COMMAND ----------
+
+// MAGIC %python
+// MAGIC 
+// MAGIC df = table("joins_smoothed") \
+// MAGIC   .groupBy("queryName", "sparkVersion") \
+// MAGIC   .agg( \
+// MAGIC     avg("medianRuntimeSeconds").alias("runtime"), \
+// MAGIC     expr("stddev(medianRuntimeSeconds)").alias("error")) \
+// MAGIC   .withColumn("num", expr("getNum(queryName)")) \
+// MAGIC   .withColumn("variant", expr("getVariant(queryName)")) \
+// MAGIC   .withColumn("param", expr("getParam(queryName)")) \
+// MAGIC   .orderBy("num", "param") \
+// MAGIC   .toPandas()
+// MAGIC 
+// MAGIC bar_chart(df, graphCol = 'variant', groupCol='param', colorCol='sparkVersion')
+
+// COMMAND ----------
+
+display(
+  joins
+    .baseData
+    .where($"sparkVersion" === "1.6.0" && $"queryName" === "join - numMatches: 16")
+    .select($"runId", $"iteration", $"runtimeSeconds")
+    .orderBy("iteration", "runId"))
+
+// COMMAND ----------
+
+// MAGIC %python
+// MAGIC 
+// MAGIC df = table("joins_smoothed") \
+// MAGIC   .groupBy("queryName", "sparkVersion") \
+// MAGIC   .agg( \
+// MAGIC     avg("medianRuntimeSeconds").alias("runtime"), \
+// MAGIC     expr("stddev(medianRuntimeSeconds)").alias("error")) \
+// MAGIC   .withColumn("num", expr("getNum(queryName)")) \
+// MAGIC   .withColumn("variant", expr("getVariant(queryName)")) \
+// MAGIC   .withColumn("param", expr("getParam(queryName)")) \
+// MAGIC   .orderBy("num", "param") \
+// MAGIC 
+// MAGIC display(df)
+// MAGIC df = df.toPandas()
+
+// COMMAND ----------
+
+// MAGIC %python
+// MAGIC 
+// MAGIC df = table("joins_smoothed") \
+// MAGIC   .where("runId >= -1")
+// MAGIC   .groupBy("queryName", "sparkVersion") \
+// MAGIC   .agg( \
+// MAGIC     avg("medianRuntimeSeconds").alias("runtime"), \
+// MAGIC     expr("stddev(medianRuntimeSeconds)").alias("error")) \
+// MAGIC   .withColumn("num", expr("getNum(queryName)")) \
+// MAGIC   .withColumn("variant", expr("getVariant(queryName)")) \
+// MAGIC   .withColumn("param", expr("getParam(queryName)")) \
+// MAGIC   .orderBy("num", "param") \
+// MAGIC   .toPandas()
+// MAGIC 
+// MAGIC colors = ['r', 'g', 'b']
+// MAGIC 
+// MAGIC variants = df['variant'].unique()
+// MAGIC num_variants = len(variants)
+// MAGIC rows = int(ceil(num_variants / 2))
+// MAGIC 
+// MAGIC #fig = plt.figure()
+// MAGIC fig, axarr = plt.subplots(num_variants, figsize=(5, 15))
+// MAGIC 
+// MAGIC for idx, variant in enumerate(variants):
+// MAGIC   ax = axarr[idx]
+// MAGIC   data = df[df['variant'] == variant]
+// MAGIC 
+// MAGIC   ax.set_title(variant)
+// MAGIC   nums = list(data['param'].unique())
+// MAGIC   N = len(nums)
+// MAGIC   width = 0.3       # the width of the bars
+// MAGIC   ind = np.arange(N)  # the x locations for the groups
+// MAGIC   ax.set_xticklabels( nums )
+// MAGIC   ax.set_xticks(np.arange(N) + (width * 1.5))
+// MAGIC 
+// MAGIC   versions = sorted(list(df['sparkVersion'].unique()))
+// MAGIC   rects = []
+// MAGIC   for i, v in enumerate(versions):
+// MAGIC     data = df[df['sparkVersion'] == v][df['variant'] == variant]
+// MAGIC     rect = ax.bar(ind, list(data['runtime']), width, color=colors[i], yerr=list(data['error']), ecolor='black')
+// MAGIC     rects.append(rect)
+// MAGIC     ind = ind + width
+// MAGIC 
+// MAGIC   ax.legend( rects, versions, loc='upper left')
+// MAGIC 
+// MAGIC display(fig)
+
+// COMMAND ----------
+
+// MAGIC %python
+// MAGIC 
+// MAGIC df = table("joins_smoothed") \
+// MAGIC   .groupBy("queryName", "sparkVersion") \
+// MAGIC   .agg( \
+// MAGIC     avg("medianRuntimeSeconds").alias("runtime"), \
+// MAGIC     expr("stddev(medianRuntimeSeconds)").alias("error")) \
+// MAGIC   .withColumn("num", expr("getNum(queryName)")) \
+// MAGIC   .withColumn("variant", expr("getVariant(queryName)")) \
+// MAGIC   .withColumn("param", expr("getParam(queryName)")) \
+// MAGIC   .orderBy("num", "param") \
+// MAGIC   .toPandas()
+// MAGIC 
+// MAGIC def bar_chart(df, graphCol = 'variant', groupCol = 'param', colorCol = 'sparkVersion', heightCol = 'runtime', colors = ['#29B1C1', '#EF6C30', '#21B16B', '#E8302D']):
+// MAGIC   graphs = df[graphCol].unique()
+// MAGIC   num_graphs = len(graphs)
+// MAGIC   rows = int(ceil(num_graphs / 2))
+// MAGIC 
+// MAGIC   fig, axarr = plt.subplots(num_graphs, figsize=(5, 15))
+// MAGIC 
+// MAGIC   for idx, graph in enumerate(graphs):
+// MAGIC     ax = axarr[idx]
+// MAGIC     data = df[df[graphCol] == graph]
+// MAGIC 
+// MAGIC     ax.set_title(graph)
+// MAGIC     groups = list(data[groupCol].unique())
+// MAGIC     num_groups = len(groups)
+// MAGIC     width = 0.3                  # the width of the bars
+// MAGIC     ind = np.arange(num_groups)  # the x locations for the groups
+// MAGIC     ax.set_xticklabels(groups)
+// MAGIC     ax.set_xticks(np.arange(num_groups) + (width * 1.5))
+// MAGIC 
+// MAGIC     versions = sorted(list(df[colorCol].unique()))
+// MAGIC     rects = []
+// MAGIC     for i, v in enumerate(versions):
+// MAGIC       data = df[df[colorCol] == v][df[graphCol] == graph]
+// MAGIC       rect = ax.bar(ind, list(data[heightCol]), width, color=colors[i], yerr=list(data['error']), ecolor='black')
+// MAGIC       rects.append(rect)
+// MAGIC       ind = ind + width
+// MAGIC 
+// MAGIC     ax.legend( rects, versions, loc='upper left')
+// MAGIC 
+// MAGIC   display(fig)
+// MAGIC 
+// MAGIC bar_chart(df)
+
+// COMMAND ----------
+
 // MAGIC %md # Query Plan Breakdown
 
 // COMMAND ----------
 
-val result = tpcds1500.baseData.where($"queryName" === getArgument("queryName", "ss_max")).groupBy("sparkVersion").agg(first($"result.queryExecution")).orderBy("sparkVersion").collect().map {
+val queries = tpcds1500.baseData
+  .where($"queryName" === getArgument("queryName", ""))
+  .groupBy("sparkVersion")
+  .agg(first($"result.queryExecution"))
+  .orderBy("sparkVersion")
+
+val result = queries.collect().map {
   case Row(version: String, queryExecution: String) =>
     s"""
      |<h1>$version</h1>
